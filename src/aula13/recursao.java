026bf5ce2a1afe4307aafaa5d15c25e55c5990b5
wrkerber <wrkerber@LAB7-36-WIN.ROCHAS>
2018-05-22 22:06:26 -0300

    Aula 13 - recursão

--- src/aula13/recursao.java
+++ src/aula13/recursao.java
@@ -44,9 +44,54 @@ public class recursao
             return fibonacci(n - 1) + fibonacci(n - 2); // caso geral
         }
     }
+    
+    public static double sum (double[] v, int i)
+    {
+        if (i == v.length - 1) // Caso de parada
+        {
+            return v[i];
+        }
+        else // Caso geral
+        {
+            return v[i] + sum(v, i + 1);
+        }
+    }
+    
+    public static void binary (int n, String s)
+    {
+        if (n == 0)
+        {
+            System.out.println(s);
+        }
+        else
+        {
+            binary(n - 1, s + '0');
+            binary(n - 1, s + '1');
+        }
+    }
+    
+    public static void abcde (int n, String s)
+    {
+        if (n == 0)
+        {
+            System.out.println(s);
+        }
+        else
+        {
+            abcde(n - 1, s + 'a');
+            abcde(n - 1, s + 'b');
+            abcde(n - 1, s + 'c');
+            abcde(n - 1, s + 'd');
+            abcde(n - 1, s + 'e');
+        }
+    }
 
     public static void main(String[] args)
     {
-        System.out.println(fatorial(20));
+        //System.out.println(fatorial(20));
+        // double[] x = {1.2, 3.4, 5.6, 7.8};
+        // System.out.println(sum(x, 0));
+        //binary(3, "");
+        abcde(4, "");
     }
 }
